9d5600d8ce67d54d96af15515054c262372e3a8b
内藤 陽介(Yosuke Naito) <yosuke.naito@metaphysicalclub.org>
2020-05-14 13:14:25 +0900

    Corrected some bugs.

--- www/controllers/IndexController.php
+++ www/controllers/IndexController.php
@@ -4,13 +4,14 @@ require_once("../controllers/Controller.php");
 class IndexController extends Controller {
     public function indexGET() {
         if (!$this->checkLogin()) {
+            error_log("？");
             $this->delegateToLoginController();
 
             return;
         }
 
         $smarty = $this->getSmarty();
-        $smarty->display("LoginViewIndexGET.html");
+        $smarty->display("LoginViewIndexPOST.html");
     }
 }
 ?>
